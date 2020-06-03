d4c28ec7758fa4de6d110694f8747f48a947c7e9
Ubuntu <ubuntu@ip-172-31-86-208.ec2.internal>
2020-06-03 02:37:13 +0000

    studies v2

--- studies/hoisting.js
+++ studies/hoisting.js
@@ -6,28 +6,35 @@
  * to the top of the code at call time. This allows the devs to be able to use either a function
  * or a variable at any time with in the code. 
  * 
- * Variables:   Only hoist the declaration not the value
- * var:         can be called before its assigned a value. If called it will return
- *              undefined.
- * let / cont:  both are hoisted but can't be called before assignment. Will throw
- *              a reference error.
+ * Variables:   
+ *      Only hoist the declaration not the value
+ * var:         
+ *      can be called before its assigned a value. If called it will return
+ *      undefined.
  * 
- * Functions:   Hoist both the declaration and the assigned value if assigned with 
- *              a function declaration. If assigned with a function expression only
- *              the declaration is hoisted the code block is not. This functions
- *              similar to a variable. We will see examples below.
+ * let / cont:  
+ *      both are hoisted but can't be called before assignment. Will throw
+ *      a reference error.
  * 
- * Temporal Dead Zone:  the engine actually goes through the code twice. once  at
- *                      the creation phase, it'll take all the variables and functions
- *                      and store them in memory if it was declared with var it'll assign
- *                      it undefined but if if it was declared with let / const then it'll
- *                      still hoist it but have a special mode temporal dead zone where
- *                      they exist but you don't have access to them until declared.
+ * Functions:   
+ *      Hoist both the declaration and the assigned value if assigned with 
+ *      a function declaration. If assigned with a function expression only
+ *      the declaration is hoisted the code block is not. This functions
+ *      similar to a variable. We will see examples below.
  * 
- * JS Interpreter:  or engine. Basically a way for javascript to be converted to 
- *                  machine understand that then is able to operate on the machine. It is
- *                  required for your computer to understand what you are trying to do.
- *                  Converts high-level source code into low-level machine code.
+ * Temporal Dead Zone:  
+ *      the engine actually goes through the code twice. once  at
+ *      the creation phase, it'll take all the variables and functions
+ *      and store them in memory if it was declared with var it'll assign
+ *      it undefined but if if it was declared with let / const then it'll
+ *      still hoist it but have a special mode temporal dead zone where
+ *      they exist but you don't have access to them until declared.
+ * 
+ * JS Interpreter:  
+ *      or engine. Basically a way for javascript to be converted to 
+ *      machine understand that then is able to operate on the machine. It is
+ *      required for your computer to understand what you are trying to do.
+ *      Converts high-level source code into low-level machine code.
 */
 
 
